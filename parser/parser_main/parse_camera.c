8702c6bb1ebf07b3c2fb55524df514bcd6eca9aa
luukisaiah <lderks@codam.student.nl>
2026-04-21 17:29:37 +0200

    final final changes u get me

--- parser/parser_main/parse_camera.c
+++ parser/parser_main/parse_camera.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   parse_camera.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: doda-cun <doda-cun@student.codam.nl>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/10 18:19:54 by doda-cun          #+#    #+#             */
-/*   Updated: 2026/04/09 18:35:03 by doda-cun         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   parse_camera.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lderks <lderks@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/03/10 18:19:54 by doda-cun      #+#    #+#                 */
+/*   Updated: 2026/04/21 17:18:10 by lderks        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	parse_camera(char *line, t_scene *scene)
 	scene->camera.pos = parse_vector(parts[1]);
 	scene->camera.dir = parse_normal(parts[2]);
 	scene->camera.fov = safe_atoi(parts[3]);
+	if (scene->camera.fov == 0)
+		scene->camera.fov = 1;
+	else if (scene->camera.fov == 180)
+		scene->camera.fov = 179;
 	check_fov(scene->camera.fov);
 	free_split(parts);
 	scene->parser.has_camera = 1;
